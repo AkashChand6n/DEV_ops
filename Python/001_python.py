3dda858af4290cf1d82abcb2c872554f54b33c2a
Akashchand6n <akashanoop123456@gmail.com>
2025-02-04 12:15:22 +0530

    Python day 1

---
+++ Python/001_python.py
@@ -0,0 +1,28 @@
+# first="Akash"
+# last="Chandran"
+# age=24 
+# print(first)
+# print(last)
+# print(age)
+
+
+# preinfo={'first':'Akash','last':'chandran','age':'24'} 
+# print(preinfo)
+# print(len(first))
+
+
+# x=5
+# # x//=3
+# # print(x)
+
+# x&=3
+# print(x)
+
+# lenght=input("Enter the lenght:")
+# width=input("Enter the width")
+# # lenght=float(lenght)
+# # width=float(width)
+# print("the area is ", float(lenght*width))
+
+# print((3<=2) or (4>=1))
+
---
+++ Python/002_string.py
@@ -0,0 +1,9 @@
+# multilinestring='''Hello world,
+# this is you to remember that you are awsome'''
+# print(multilinestring)
+
+# first="Akash"
+# last="chandran"
+# print(first+' '+last)
+# print(len(first+last))
+
