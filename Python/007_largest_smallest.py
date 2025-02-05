9b7c8fe456d912212a0e5876534efd20a3b67406
Akashchand6n <akashanoop123456@gmail.com>
2025-02-05 09:56:00 +0530

    Max and min of number in list

---
+++ Python/007_largest_smallest.py
@@ -0,0 +1,11 @@
+list=["22","33","44","55","66","77","88","99"]
+print("The largest number in the list is:",max(list))
+print("The smallest number in the list is:",min(list))
+#min and max functions are used to find the smallest and largest number in the list
+
+
+sorted_list=sorted(list)
+print("The sorted list is:",sorted_list)
+print("max of list is:",sorted_list[-1])
+print("min of list is:",sorted_list[0])
+#sorted() function returns a new list of sorted elements
