00d160086339bf886f99fb1923339af51ebe6363
Akashchand6n <akashanoop123456@gmail.com>
2025-02-05 12:48:10 +0530

    updated string slicing

---
+++ Python/010_string_slicing.py
@@ -0,0 +1,21 @@
+str=["akash","sanju","devan","abhirami","selmi"]
+# print(str[1:]) #prints all elements from index 1
+# print(str[:3]) #prints all elements upto index 3
+
+#.append() method is used to add an element to the end of the list
+#.insert() method is used to add an element at a specific index
+#.remove() method is used to remove an element from the list
+#.pop() method is used to remove an element from a specific index
+#.clear() method is used to remove all elements from the list
+#.copy() method is used to copy the list
+#.concatenation operator (+) is used to concatenate two lists
+#.extend() method is used to add elements of one list to another list
+#.count() method is used to count the number of times an element appears in the list
+#.index() method is used to find the index of an element in the list
+
+str2=["sree","surya","sreejith","sreejith"]
+print(str)
+print(str2)
+
+str.insert(0,"sreejith") #inserts "sreejith" at index 0
+print(str)
