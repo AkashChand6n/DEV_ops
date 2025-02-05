c73eb9d59759fa64d1a32adfbd08fcfdbde6ce70
Akashchand6n <akashanoop123456@gmail.com>
2025-02-05 14:43:17 +0530

    tuples

---
+++ Python/011.tuples.py
@@ -0,0 +1,8 @@
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+print(weekdays)
+print(weekdays[1:]) #prints all elements from index 1
+print(weekdays[:3]) #prints all elements upto index 3
+print (weekdays.count("Wednesday")) #prints the number of times "akash" appears in the tuple
+print (weekdays.index("Friday")+1) #prints the index of "akash" in the tuple
+
+print(list(weekdays)) #converts the tuple to a list
