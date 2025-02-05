a3b40d658f25336ac7cef7a55fe5396a453eb81a
Akashchand6n <akashanoop123456@gmail.com>
2025-02-05 09:55:32 +0530

    sort on list

---
+++ Python/006_list_of_fruits.py
@@ -0,0 +1,9 @@
+# List of fruits and sorting them in alphabetical order
+fruits = ["apple", "date", "fig", "kiwi", "banana"]
+fruits.sort(reverse=True)
+print(fruits)
+
+#sorted() function returns a new list of sorted elements
+#fruit.sort() method sorts the list in place
+#fruit[::-1] reverses the list
+#fruit.sort(reverse=True) sorts the list in reverse order
