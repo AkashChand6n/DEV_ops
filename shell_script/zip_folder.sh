c5de41fd7f8563273c116cbdb02f6173906c7144
Akashchand6n <akashanoop123456@gmail.com>
2025-02-06 12:20:37 +0530

    Added zip_folder.sh script

--- shell_script/zip_folder.sh
+++ shell_script/zip_folder.sh
@@ -2,7 +2,7 @@ echo "enter the directory"
 read -r dir
 if [ -d $dir ]; then
     echo "directory exists"
-    zip -r "$dir".zip "$dir"
+    zip -r "$dir".zip "$dir" # -r flag is used to recursively zip the directory
 else
     echo "directory does not exist"
     exit 1
