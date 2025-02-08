cdfc5c7a7552c6f27a4b85f1d67788ae46985163
Akashchand6n <akashanoop123456@gmail.com>
2025-02-08 10:44:24 +0530

    added if else, elif

---
+++ Python/014_if_else_elif.py
@@ -0,0 +1,7 @@
+n = int(input("enter the number:"))
+if n > 10:
+    print("the number is grater then 10")
+elif n == 10:
+    print("the number is equal to 10")
+else:
+    print("the number is less than 10")
