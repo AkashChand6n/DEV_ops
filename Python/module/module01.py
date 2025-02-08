63651335e15e93378dd84f98911fdc7e0c89e57d
Akashchand6n <akashanoop123456@gmail.com>
2025-02-08 14:24:52 +0530

    Added module in python

---
+++ Python/module/module01.py
@@ -0,0 +1,7 @@
+#write a code for finding prime number
+def name(first,last):
+    return first+" "+last
+
+def sum(a,b):
+    return a+b
+
---
+++ Python/module/module02.py
@@ -0,0 +1,3 @@
+from module01 import sum as Sum , name as Name
+print(Sum(10,20))
+print(Name("sachin","yadav"))
